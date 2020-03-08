a90074266d6111bec5a3f5cc9adb7372b80052b7
Sebastian Vesga Mac <svesgamartinez@breg.com>
2020-03-07 21:58:02 -0800

    Added SNS notification

--- aws_serverless.py
+++ aws_serverless.py
@@ -5,25 +5,28 @@ import zipfile
 import mimetypes 
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:802992738993:CodeDeployTopic') 
     
-    portafolio_bucket = s3.Bucket('portafolio.vesgasebas.net')
-    portafoliobuild_bucket = s3.Bucket('portafoliobuild.vesgasebas.net')
+    try:
+        s3 = boto3.resource('s3')
+        portafolio_bucket = s3.Bucket('portafolio.vesgasebas.net')
+        portafoliobuild_bucket = s3.Bucket('portafoliobuild.vesgasebas.net')
+        
+        portafolio_zip = StringIO.StringIO()
+        portafoliobuild_bucket.download_fileobj('CodeBuild_Portafolio_VesgaSebas.zip', portafolio_zip)
     
-    portafolio_zip = StringIO.StringIO()
-    portafoliobuild_bucket.download_fileobj('CodeBuild_Portafolio_VesgaSebas.zip', portafolio_zip)
-    
-    with zipfile.ZipFile(portafolio_zip) as myzip:  
-        for nm in myzip.namelist(): 
-            obj = myzip.open(nm) 
-            portafolio_bucket.upload_fileobj(obj, nm, 
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]}) 
-            portafolio_bucket.Object(nm).Acl().put(ACL='public-read')
-        # TODO implement
-    return {
-            'statusCode': 200,
-            'body': json.dumps('Hello from Lambda!')
-        }
+        with zipfile.ZipFile(portafolio_zip) as myzip:  
+            for nm in myzip.namelist(): 
+                obj = myzip.open(nm) 
+                portafolio_bucket.upload_fileobj(obj, nm) 
+                portafolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print('Job Completed')
+        topic.publish(Subject="Deploy Portafolio Vesgasebas.Net - Completed", Message="Lambda was able to deploy the code to s3") 
+    except: 
+        topic.publish(Subject="Deploy Portafolio Vesgasebas.Net - Failed", Message="Lambda was UNABLE to deploy the code to s3") 
+        raise
+    return "Hello Lambda"
 
 
 """ import boto3 
